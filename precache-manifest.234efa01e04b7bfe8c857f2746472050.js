a1b17cff8cdb234503bcfa6c6083d85f5e243b8d
teye <teye@users.noreply.github.com>
2023-10-24 06:30:32 +0000

    deploy: ae8ba4351a14952bc2ccec648c31ce5a6837a39c

---
+++ precache-manifest.234efa01e04b7bfe8c857f2746472050.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "3336f142df079c2ebb39",
+    "url": "/snake-game-blockchain/static/css/main.ece1fa49.chunk.css"
+  },
+  {
+    "revision": "3336f142df079c2ebb39",
+    "url": "/snake-game-blockchain/static/js/main.8826e461.chunk.js"
+  },
+  {
+    "revision": "78b492eb1e78d8c06066",
+    "url": "/snake-game-blockchain/static/js/runtime~main.0b4a9ec2.js"
+  },
+  {
+    "revision": "96a050f92dfb1a72fe0f",
+    "url": "/snake-game-blockchain/static/js/2.8c95b90b.chunk.js"
+  },
+  {
+    "revision": "863d2856ef76e7a9b716",
+    "url": "/snake-game-blockchain/static/js/3.d9893524.chunk.js"
+  },
+  {
+    "revision": "237d35f81f27540e3be0511250de8513",
+    "url": "/snake-game-blockchain/static/media/garden_snake.237d35f8.png"
+  },
+  {
+    "revision": "d0dc6517b1c91225cf2c731a7e283524",
+    "url": "/snake-game-blockchain/static/media/viper.d0dc6517.png"
+  },
+  {
+    "revision": "5d95b8fb86ecd0340c6024c6d8063284",
+    "url": "/snake-game-blockchain/static/media/king_cobra.5d95b8fb.png"
+  },
+  {
+    "revision": "ab3bb084d16cd67fae2d2c075e4a17eb",
+    "url": "/snake-game-blockchain/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/snake-game-blockchain/precache-manifest.a29fb3c859119ab4e6a9e95540b542bd.js"
+  "/snake-game-blockchain/precache-manifest.234efa01e04b7bfe8c857f2746472050.js"
 );
 
 workbox.clientsClaim();
